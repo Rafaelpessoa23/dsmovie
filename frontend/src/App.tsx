a5af7689ab7a38886862895b3e9a87d9111c590f
Rafael Pessoa <rafaelpessoa@id.uff.br>
2022-02-12 16:13:14 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+
+import Form from 'components/Navbar/Pages/form';
+import Listing from 'components/Navbar/Pages/listing';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-    </>
-
+      <Routes>
+        <Route path='/' element={<Listing />} />
+        <Route path='/form'>
+          <Route path=':movieId' element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
